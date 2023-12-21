074af15a512a6f7e19904111422e4bdb772ebfee
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2023-12-21 17:19:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
+
+
+class InferlessPythonModel:
+    def initialize(self):
+        config = PeftConfig.from_pretrained("G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231219-1611")
+        self.model = AutoModelForCausalLM.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
+        self.model = PeftModel.from_pretrained(model, "G-ML-Hyly/stg-cli13b-t6-cdp-ca.mt.him.cln.inter-b4s1e1-20231219-1611")
+        self.model.to("cuda:0")
+        self.tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
+    
+    def infer(self, inputs):
+        prompt = inputs["prompt"]
+        batch = tokenizer(prompt, return_tensors='pt').to("cuda:0")
+        output_tokens = model.generate(**batch, max_new_tokens=50)
+        output = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+        return {"generated_output": output}
+
+    def finalize(self,args):
+        self.model = None
+        self.tokenizer = None
